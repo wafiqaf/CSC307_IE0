0df7848d7d50f25223b6aba7aad6c02bf7fb8dfa
wafiqaf <wafiqafarhana@gmail.com>
2024-04-12 09:26:34 -0700

    Finished step 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,9 @@ const findUserByName = (name) => {
     );
   };
 
+  const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -54,6 +57,16 @@ app.get("/users", (req, res) => {
       res.send(result);
     } else {
       res.send(users);
+    }
+  });
+
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
     }
   });
 
